73f125c2d0208795bf3e2eed05e0b4ba8d0304c8
cjmazzarri <cjmazzarri@gmail.com>
2021-04-15 11:44:41 -0500

    :sparkles: Finished user class.

--- src/main/java/com/speedplanner/model/User.java
+++ src/main/java/com/speedplanner/model/User.java
@@ -1,5 +1,35 @@
 package com.speedplanner.model;
 
-public class User {
+import lombok.Data;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @NotNull
+    @Size(max = 20)
+    private String username;
+
+    @NotBlank
+    @NotNull
+    @Size(max = 30)
+    private String password;
+
+    @NotBlank
+    @NotNull
+    @Size(max = 40)
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "profile_id", referencedColumnName = "id")
+    private Profile profile;
 }
