968a61e9e88b0e338c162cca86bca6212975e52c
Marcelo <49617031+vilaron@users.noreply.github.com>
2021-04-15 14:59:50 -0500

    ✨Added Simple Tasks Resource

---
+++ src/main/java/com/speedplanner/resource/SaveSimpleTasksResource.java
@@ -0,0 +1,30 @@
+package com.speedplanner.resource;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Lob;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Getter
+@Setter
+public class SaveSimpleTasksResource {
+    @NotNull
+    private boolean finished;
+
+    @NotNull
+    private Date deadline;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 100)
+    private String title;
+
+    @NotNull
+    @Lob
+    private String description;
+}
