34ad8fe1daa2b593e978f6424463146a4afc48c0
cjmazzarri <cjmazzarri@gmail.com>
2021-04-15 13:03:25 -0500

    ✨Added User Service Implementation

--- src/main/java/com/speedplanner/service/UserServiceImpl.java
+++ src/main/java/com/speedplanner/service/UserServiceImpl.java
@@ -1,4 +1,50 @@
 package com.speedplanner.service;
 
-public class UserServiceImpl {
+import com.speedplanner.exception.ResourceNotFoundException;
+import com.speedplanner.model.User;
+import com.speedplanner.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId).orElseThrow(
+                () -> new ResourceNotFoundException("User not found with Id: "+userId));
+    }
+
+    @Override
+    public Page<User> getAllUsers(Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+    @Override
+    public User updateUser(Long userId, User userRequest) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new ResourceNotFoundException("User not found with Id: "+userId)
+        );
+        user.setUsername(userRequest.getUsername());
+        user.setPassword(userRequest.getPassword());
+        user.setEmail(userRequest.getEmail());
+        return userRepository.save(userRequest);
+    }
+
+    @Override
+    public ResponseEntity<?> deleteUser(Long userId) {
+        return userRepository.findById(userId).map(user -> {
+            userRepository.delete(user);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException(
+                "User not found with Id: " + userId));
+    }
 }
