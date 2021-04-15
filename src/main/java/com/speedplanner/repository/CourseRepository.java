9f11298332df754256bf234876d7fbff0ef66e86
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-04-15 12:16:07 -0500

    :sparkles: Added Course Repository

--- src/main/java/com/speedplanner/repository/CourseRepository.java
+++ src/main/java/com/speedplanner/repository/CourseRepository.java
@@ -1,4 +1,9 @@
 package com.speedplanner.repository;
 
-public interface CourseRepository {
+import com.speedplanner.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
 }
