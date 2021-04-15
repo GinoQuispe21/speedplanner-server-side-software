62a77cae23fa34934d609cba8c8b2f05291538b1
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-04-15 10:35:06 -0500

    Started Project :tada:

---
+++ src/main/java/com/speedplanner/SpeedplannerApplication.java
@@ -0,0 +1,13 @@
+package com.speedplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedplannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/speedplanner/SpeedplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speedplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedplannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
