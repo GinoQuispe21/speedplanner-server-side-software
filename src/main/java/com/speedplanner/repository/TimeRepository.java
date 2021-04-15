b7cbfd107a5fe6b635647a7f3826b1393cdc0dfe
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-04-15 12:16:19 -0500

    :sparkles: Added Time Repository

---
+++ src/main/java/com/speedplanner/repository/TimeRepository.java
@@ -0,0 +1,9 @@
+package com.speedplanner.repository;
+
+import com.speedplanner.model.Time;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TimeRepository extends JpaRepository<Time, Long> {
+}
