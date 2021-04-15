b51268edbbf499304fc252b93eef90d94f2d9ab5
cjmazzarri <cjmazzarri@gmail.com>
2021-04-15 12:28:28 -0500

    ✨Added UserService

--- src/main/java/com/speedplanner/service/UserService.java
+++ src/main/java/com/speedplanner/service/UserService.java
@@ -1,4 +1,14 @@
 package com.speedplanner.service;
 
+import com.speedplanner.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
 public interface UserService {
+    User createUser(User user);
+    User getUserById(Long userId);
+    Page<User> getAllUsers(Pageable pageable);
+    User updateUser(Long userId, User userRequest);
+    ResponseEntity<?> deleteUser(Long userId);
 }
