91243a2c7d5fcfaf6abaeeb29fdb9ef66adf5557
cjmazzarri <cjmazzarri@gmail.com>
2021-04-15 12:11:18 -0500

    ✨Added User Resource

--- src/main/java/com/speedplanner/resource/UserResource.java
+++ src/main/java/com/speedplanner/resource/UserResource.java
@@ -1,4 +1,11 @@
 package com.speedplanner.resource;
 
+import lombok.Data;
+
+@Data
 public class UserResource {
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
 }
