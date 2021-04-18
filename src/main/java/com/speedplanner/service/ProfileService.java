1adb508bc06af519b21dee4c44723ced404c7a53
cjmazzarri <cjmazzarri@gmail.com>
2021-04-18 18:12:52 -0500

    🔥Removed unnecesary method and respective imports.

--- src/main/java/com/speedplanner/service/ProfileService.java
+++ src/main/java/com/speedplanner/service/ProfileService.java
@@ -1,14 +1,10 @@
 package com.speedplanner.service;
 
 import com.speedplanner.model.Profile;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
 public interface ProfileService {
     Profile createProfile(Long userId, Profile profile);
     Profile getProfileById(Long id);
-    Page<Profile> getAllProfiles(Pageable pageable);
     Profile updateProfile(Long profileId, Profile profileRequest);
-    ResponseEntity<?> deleteProfile(Long id);
 }
