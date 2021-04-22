d3f0ac54ec274503460d3ac7d5b544d0b7e4c605
cjmazzarri <cjmazzarri@gmail.com>
2021-04-22 16:23:54 -0500

    🐛Added missing imports

--- src/main/java/com/speedplanner/controller/TimedTaskController.java
+++ src/main/java/com/speedplanner/controller/TimedTaskController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
