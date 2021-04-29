12eccf4be7b2ddfe30d8805b09fc76979220ddb1
cjmazzarri <cjmazzarri@gmail.com>
2021-04-29 01:38:49 -0500

    🔥Removed unused import.

--- src/main/java/com/speedplanner/service/ProfileServiceImpl.java
+++ src/main/java/com/speedplanner/service/ProfileServiceImpl.java
@@ -6,7 +6,6 @@ import com.speedplanner.model.User;
 import com.speedplanner.repository.ProfileRepository;
 import com.speedplanner.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
