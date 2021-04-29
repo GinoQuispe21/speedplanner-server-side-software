a1a160d03b4124d80c83de693038653f8d5039ea
cjmazzarri <cjmazzarri@gmail.com>
2021-04-29 01:37:59 -0500

    ♻️Renamed from plural to singular.

--- src/main/java/com/speedplanner/service/TimedTaskServiceImpl.java
+++ src/main/java/com/speedplanner/service/TimedTaskServiceImpl.java
@@ -2,7 +2,7 @@ package com.speedplanner.service;
 
 import com.speedplanner.exception.ResourceNotFoundException;
 import com.speedplanner.model.TimedTask;
-import com.speedplanner.repository.TimedTasksRepository;
+import com.speedplanner.repository.TimedTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,43 +10,44 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TimedTasksServiceImp implements  TimedTasksService{
+public class TimedTaskServiceImpl implements TimedTaskService {
 
     @Autowired
-    private TimedTasksRepository TimedTasksRepository;
+    private TimedTaskRepository TimedTaskRepository;
 
     @Override
     public Page<TimedTask> getAllTimedTasks(Pageable pageable) {
-        return TimedTasksRepository.findAll(pageable);
+        return TimedTaskRepository.findAll(pageable);
     }
 
     @Override
+    //TODO: Course logic
     public TimedTask createTimedTask(TimedTask TimedTask) {
-        return TimedTasksRepository.save(TimedTask);
+        return TimedTaskRepository.save(TimedTask);
     }
 
     @Override
     public TimedTask getTimedTaskById(Long Id) {
-        return TimedTasksRepository.findById(Id).orElseThrow(
+        return TimedTaskRepository.findById(Id).orElseThrow(
                 () -> new ResourceNotFoundException("Timed Task not found with Id " + Id ));
     }
 
     @Override
     public TimedTask updateTimedTask(Long TimedTaskId, TimedTask TimedTaskRequest) {
-        TimedTask timedTask = TimedTasksRepository.findById(TimedTaskId).orElseThrow(
+        TimedTask timedTask = TimedTaskRepository.findById(TimedTaskId).orElseThrow(
                 () -> new ResourceNotFoundException("Timed Task not found with Id " + TimedTaskId ));
         timedTask.setTitle(TimedTaskRequest.getTitle());
         timedTask.setDescription(TimedTaskRequest.getDescription());
         timedTask.setFinished(TimedTaskRequest.isFinished());
         timedTask.setFinish_time(TimedTaskRequest.getFinish_time());
         timedTask.setStart_time(TimedTaskRequest.getStart_time());
-        return TimedTasksRepository.save(timedTask);
+        return TimedTaskRepository.save(timedTask);
     }
 
     @Override
     public ResponseEntity<?> deleteTimedTask(Long TimedTaskId) {
-        TimedTask timedTask = TimedTasksRepository.findById(TimedTaskId).orElseThrow(() -> new ResourceNotFoundException("Timed Task" , "Id" , TimedTaskId));
-        TimedTasksRepository.delete(timedTask);
+        TimedTask timedTask = TimedTaskRepository.findById(TimedTaskId).orElseThrow(() -> new ResourceNotFoundException("Timed Task" , "Id" , TimedTaskId));
+        TimedTaskRepository.delete(timedTask);
         return ResponseEntity.ok().build();
     }
 }
