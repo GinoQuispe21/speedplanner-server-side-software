2f34f7ea59ac45ba92ae228148f78ceb9082b939
cjmazzarri <cjmazzarri@gmail.com>
2021-04-29 01:42:16 -0500

    ♻️Renamed from plural to singular.

--- src/main/java/com/speedplanner/repository/SimpleTaskRepository.java
+++ src/main/java/com/speedplanner/repository/SimpleTaskRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SimpleTasksRepository extends JpaRepository<SimpleTask, Long> {
+public interface SimpleTaskRepository extends JpaRepository<SimpleTask, Long> {
 }
