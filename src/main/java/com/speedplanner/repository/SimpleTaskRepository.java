ddf18833d2d76116f2c90428742d304621d6dd4c
cjmazzarri <cjmazzarri@gmail.com>
2021-05-06 11:37:53 -0500

    ✨Added findByCourseId methods.

--- src/main/java/com/speedplanner/repository/SimpleTaskRepository.java
+++ src/main/java/com/speedplanner/repository/SimpleTaskRepository.java
@@ -1,9 +1,13 @@
 package com.speedplanner.repository;
 
 import com.speedplanner.model.SimpleTask;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SimpleTaskRepository extends JpaRepository<SimpleTask, Long> {
+    SimpleTask findByIdAndCourseId(Long id, Long courseId);
+    Page<SimpleTask> findAllByCourseId(Long courseId, Pageable pageable);
 }
