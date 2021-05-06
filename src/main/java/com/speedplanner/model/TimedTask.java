2df68bf614f28257514cd2be610b8744ab219ca7
cjmazzarri <cjmazzarri@gmail.com>
2021-05-06 11:04:18 -0500

    ✨Added relationship annotations.

--- src/main/java/com/speedplanner/model/TimedTask.java
+++ src/main/java/com/speedplanner/model/TimedTask.java
@@ -1,6 +1,10 @@
 package com.speedplanner.model;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -39,12 +43,9 @@ public class TimedTask {
     @JoinColumn(name = "notification_id", referencedColumnName = "id")
     private Notification notification;
 
-    /*TODO:
-    courses
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "course_id" , nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonIgnore
     private Course course;
-
-     */
 }
