3ec434c894a37eb131bbf4272472a0017dfb71b2
cjmazzarri <cjmazzarri@gmail.com>
2021-05-06 14:22:18 -0500

    🐛Added parameters for StudyGroup logic.

--- src/main/java/com/speedplanner/service/TimedTaskService.java
+++ src/main/java/com/speedplanner/service/TimedTaskService.java
@@ -6,9 +6,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
 public interface TimedTaskService {
-    Page<TimedTask> getAllTimedTasks (Pageable pageable);
-    TimedTask createTimedTask (TimedTask TimedTask);
-    TimedTask getTimedTaskById (Long Id);
-    TimedTask updateTimedTask (Long TimedTaskId , TimedTask TimedTaskRequest);
-    ResponseEntity<?> deleteTimedTask (Long TimedTaskId);
+    Page<TimedTask> getAllTimedTasksByStudyGroupId(Long studyGroupId, Pageable pageable);
+    TimedTask getTimedTaskByIdAndStudyGroupId(Long studyGroupId, Long Id);
+    TimedTask createTimedTask(Long studyGroupId, TimedTask TimedTask);
+    TimedTask updateTimedTask(Long studyGroupId, Long TimedTaskId , TimedTask TimedTaskRequest);
+    ResponseEntity<?> deleteTimedTask (Long studyGroupId, Long TimedTaskId);
 }
