cf8a4758caf0d57f3481c59d51e850f05508356b
cjmazzarri <cjmazzarri@gmail.com>
2021-05-06 13:39:21 -0500

    ✨Added findById methods.

--- src/main/java/com/speedplanner/repository/TimedTaskRepository.java
+++ src/main/java/com/speedplanner/repository/TimedTaskRepository.java
@@ -1,9 +1,13 @@
 package com.speedplanner.repository;
 
 import com.speedplanner.model.TimedTask;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TimedTaskRepository extends JpaRepository<TimedTask, Long> {
+    TimedTask findByIdAndStudyGroupId(Long id, Long studyGroupId);
+    Page<TimedTask> findAllByStudyGroupId(Long studyGroupId, Pageable pageable);
 }
