b06d2f9acfabc09d1611852ae324eaf636acd989
cjmazzarri <cjmazzarri@gmail.com>
2021-05-07 01:05:02 -0500

    ✨Added CrossOrigin annotation.

--- src/main/java/com/speedplanner/controller/ProfileController.java
+++ src/main/java/com/speedplanner/controller/ProfileController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/users/{userId}")
 @Tag(name = "Profiles", description = "User API")
--- src/main/java/com/speedplanner/controller/UserController.java
+++ src/main/java/com/speedplanner/controller/UserController.java
@@ -18,6 +18,7 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 @Tag(name = "Users", description = "User API")
